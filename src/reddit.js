956deae2bb61be56c26f6351202d727c4b3b75a6
Sugeng Tigefa <me@sugeng.win>
2023-06-14 12:10:22 +0000

    :sparkles: Rabu 14 Juni 2023 19:10:22 WIB +0700

--- src/reddit.js
+++ src/reddit.js
@@ -5,7 +5,7 @@
  * @returns The url of an image or video which is cute.
  */
 export async function getCuteUrl() {
-  const response = await fetch('https://www.reddit.com/r/aww/hot.json', {
+  const response = await fetch('https://www.reddit.com/r/anime_best_moments/new.json', {
     headers: {
       'User-Agent': 'justinbeckwith:awwbot:v1.0.0 (by /u/justinblat)',
     },
